0d837b09e1d61e00a18fc75c34cc0e4cb133a81a
cdmckay <cdmckay@cdmckay.org>
2010-06-13 05:26:24 +0000

    Added ability to select and breed "parents" to produce children biomorphs.

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="user-scalable=no" />
         <title>Biomorph.js</title>
         <link rel="stylesheet" href="css/style.css" />        
-        <script type="text/javascript" src="js/lib/jquery.min.js"></script>
+        <script type="text/javascript" src="js/lib/jquery.js"></script>
+        <script type="text/javascript" src="js/lib/jquery.color.js"></script>
         <script type="text/javascript" src="js/lib/object.js"></script>
         <script type="text/javascript" src="js/lib/math.js"></script>
         <script type="text/javascript" src="js/lib/number.js"></script>
@@ -15,6 +16,17 @@
     </head>
     <body>
         <div id="body">
+            <div id="menu">
+                <div class="right-arrow"></div>
+                <div class="right-arrow"></div>
+                <div class="right-arrow"></div>
+                <div class="left-arrow"></div>
+                <div class="left-arrow"></div>
+                <div class="left-arrow"></div>
+            </div>
+            <div id="bottom-menu">
+                Shift-click to store a Biomorph.
+            </div>
             <div id="window">
                 <div class="cell"><canvas id="cell-1" width="150" height="150"></canvas></div>
                 <div class="cell"><canvas id="cell-2" width="150" height="150"></canvas></div>
@@ -27,6 +39,14 @@
                 <div class="cell"><canvas id="cell-6" width="150" height="150"></canvas></div>
                 <div class="cell"><canvas id="cell-7" width="150" height="150"></canvas></div>
                 <div class="cell"><canvas id="cell-8" width="150" height="150"></canvas></div>
+            </div>
+            <div id="save-1" class="save-cell target">
+                <div>Mother</div>
+                <canvas id="cell-save-1" width="150" height="150"></canvas>
+            </div>
+            <div id="save-2" class="save-cell">
+                <div>Father</div>
+                <canvas id="cell-save-2" width="150" height="150"></canvas>
             </div>
         </div>
     </body>
--- js/biomorph.js
+++ js/biomorph.js
@@ -42,8 +42,7 @@ var drawCreature = function(context, segments) {
         context.moveTo(start.x, -start.y);
         context.quadraticCurveTo(
         start.x + Math.signum(start.x), -start.y - Math.signum(start.y),
-        finish.x, -finish.y);
-        context.closePath();
+        finish.x, -finish.y);        
         context.stroke();
     });        
     
@@ -69,6 +68,19 @@ var mutateCreature = function(genes, index) {
     var mutated = genes.slice(0).map(function(gene) { return gene.clone() });
     mutated[index].mutate();
     return mutated;
+};
+
+var mateCreatures = function(mother, father) {
+    var child = [];
+    for (var i = 0; i < mother.length; i++) {
+        var coinFlip = Number.randomInt(0, 1);
+        if (coinFlip) {
+            child.push(mother[i]);
+        } else {
+            child.push(father[i]);
+        }
+    }
+    return child;
 };
 
 var calculateStems = function(genes) {
@@ -115,54 +127,114 @@ var createOffspring = function(parent) {
             stems: calculateStems(genes),
             dir: 0,
             oldPos: {x: 0, y: 0}
-        });
-
-        var bounds = calculateBounds(segments),
-            width = Math.max(Math.abs(bounds.min.x), bounds.max.x) * 2,
-            height = Math.max(Math.abs(bounds.min.y), bounds.max.y) * 2,
-            factor = 120 / Math.max(width, height),
-            scale = {x: factor, y: factor};
+        });        
 
         var cell = $("#cell-" + index);
         cell.parent().data("genes", genes);
         
         var context = cell[0].getContext('2d');
         context.save();
-        context.clearRect(0, 0, 150, 150);
-        context.translate(75, 75);        
-        context.scale(scale.x, scale.y);
-        context.strokeStyle = "rgb(" + genes[9].val() + ", " + genes[10].val() + ", " + genes[11].val() + ")";
-        //console.log(context.strokeStyle);
-        context.lineWidth = genes[8].val() / factor;
-        context.lineJoin = "miter";
-        context.lineCap = "square";
-        drawCreature(context, segments, scale);                
+        prepareContext(context, segments, genes);
+        drawCreature(context, segments);                
         context.restore();
     });
 
 }
 
+var prepareContext = function(context, segments, genes) {
+
+    var bounds = calculateBounds(segments),
+        width = Math.max(Math.abs(bounds.min.x), bounds.max.x) * 2,
+        height = Math.max(Math.abs(bounds.min.y), bounds.max.y) * 2,
+        factor = 120 / Math.max(width, height),
+        scale = {x: factor, y: factor};
+
+    context.clearRect(0, 0, 150, 150);
+    context.translate(75, 75);
+    context.scale(scale.x, scale.y);
+    context.strokeStyle = "rgb(" + genes[9].val() + ", " + genes[10].val() + ", " + genes[11].val() + ")";
+    context.lineWidth = genes[8].val() / factor;
+    context.lineJoin = "miter";
+    context.lineCap = "square";
+}
+
 $(function() {
 
-    //var parent = randomCreature();
-    var parent = [
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 1}),
-        bio.gene({val: 5}),
-        bio.gene({val: Number.randomInt(1, 2), min: 1, max: 2}),  // line weight
-        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}), // red
-        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}), // green
-        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}) // blue
-    ];
+    $('#menu').css('opacity', 0.3);
+    $('#bottom-menu').css('opacity', 0.9);
+
+    var parent = randomCreature();
+//    var parent = [
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 1}),
+//        bio.gene({val: 5}),
+//        bio.gene({val: Number.randomInt(1, 2), min: 1, max: 2}),  // line weight
+//        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}), // red
+//        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}), // green
+//        bio.gene({val: Number.randomInt(0, 250), min:0, max: 250, delta: 50}) // blue
+//    ];
     createOffspring(parent);
 
-    $('div.cell').click(function() {
-        createOffspring($(this).data("genes"));
+    $('div.cell').click(function(event) {
+        var genes = $(this).data("genes");
+        
+        if (event.shiftKey) {
+            var target = $('div.save-cell.target canvas')
+            target.parent().data("genes", genes);
+
+            var segments = renderCreature({
+                length: genes[7].val(),
+                stems: calculateStems(genes),
+                dir: 0,
+                oldPos: {x: 0, y: 0}
+            });
+
+            var context = target[0].getContext('2d');
+            context.save();
+            prepareContext(context, segments, genes);
+            drawCreature(context, segments);
+            context.restore();
+
+            target.parent()
+                .clearQueue()
+                .animate({'backgroundColor': '#444'}, 600)
+                .animate({'backgroundColor': '#222'}, 800);
+
+            var mateable = true;
+            saveCells.each(function() {
+                if (!$(this).data("genes")) {
+                    mateable = false;
+                }
+            });
+            if (mateable) {
+                $('#menu').fadeTo('slow', 1.0).css("cursor", "pointer");
+            }
+        } else {
+            createOffspring(genes);
+        }
+    });
+
+    var saveCells = $('div.save-cell').click(function() {
+        saveCells.removeClass('target');
+        $(this).addClass('target');        
+    });
+
+    $('#menu').click(function() {
+        var mother = $('#save-1').data("genes");
+        var father = $('#save-2').data("genes");
+        if (mother && father) {
+            var child = mateCreatures(mother, father);
+            createOffspring(child);
+            $('#cell-0').parent()
+                .clearQueue()
+                .animate({'backgroundColor': '#444'}, 600)
+                .animate({'backgroundColor': '#222'}, 800);
+        }
     });
     
 })
